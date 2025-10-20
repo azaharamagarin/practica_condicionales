b2e942ee29419373879028fc9b17495ef4d01b14
azaharamagarin <azahara.magarin@gmail.com>
2025-10-20 16:29:12 +0200

    ejercicio 2

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ function verificarNumero (num1) {
 }
 const valor=verificarNumero(5)
 console.log(valor)
+
+//ejercicio 2
+function puedevotar (num1){
+    if(num1>=18){
+        return("puede votar")
+    }else{
+        return ("no puede votar")}
+}
+const voto=puedevotar(20)
+console.log(voto)
